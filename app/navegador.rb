04beba8fd08ca393c3106e0bb435b152e8c6e5ec
Eduardo Aguilera <laloao.2302@gmail.com>
2017-03-06 00:41:18 -0600

    Horario completado

--- app/navegador.rb
+++ app/navegador.rb
@@ -1,18 +1,36 @@
 require 'mechanize'
+require_relative 'parser/horario'
 
 class Navegador
 
-  @@url = ""
+  @@url
+  @@pag_principal
+  @@pag_horario
+  @@pag_periodos
+  @@pag_creditos
+  @@matricula
+
+  @@url_terminacion
+
   @@agent
 
-  def initialize
-    @@url = "http://207.249.157.32/cgi-bin/r.cgi/Consulta/principal.r"
+  def initialize(matricula)
+
+    @@url = "http://207.249.157.32/cgi-bin/r.cgi/Consulta/"
+    @@url_principal = "principal.r"
+    @@url_terminacion = "?sistema=1&matricula="
+    @@pag_principal = "w0400501.r"
+    @@pag_horario = "w0400501.r"
+    @@pag_periodos = "w0400301.r"
+    @@pag_creditos = "w600101.r"
+    @@matricula = matricula.to_s
+
     @@agent = Mechanize.new
     @@agent.user_agent_alias = 'Mac Safari'
   end
 
   def login(params)
-    page = @@agent.get(@@url)
+    page = @@agent.get(@@url + @@url_principal)
     form = page.form('form1')
     page = @@agent.submit(form)
 
@@ -20,6 +38,18 @@ class Navegador
     form.matricula = params[:clave]
     form.nip = params[:password]
     page = @@agent.submit(form)
-    page
+    menu = page.frames[1].click
+    horario
+  end
+
+  def horario
+    url = @@url + @@pag_horario + @@url_terminacion + @@matricula
+    page = @@agent.get(url)
+
+    # Tabla que contiene las horas de las clases
+    table = page.xpath("//table")[2]
+    # arreglo que contendra todas las clases
+    clases = HorarioParser.parsear(table)
+    clases
   end
 end
---
+++ app/parser/horario.rb
@@ -0,0 +1,41 @@
+require 'nokogiri'
+
+class HorarioParser
+
+  def self.parsear(table)
+    horario = []
+    table.xpath('tr').each_with_index do |tr, i|
+      # i = 0 son las cabeceras. A partir de ahi son las clases
+      unless i == 0
+        cont = tr.content.split("\n")
+        horario += materia(cont)
+      end
+    end
+    horario
+  end
+
+  def self.materia(materia)
+    clases = []
+    nombre = materia[1]
+    # grupo = if !materia[8].empty? then materia[8] else nil end
+    # salon = if !materia[9].empty? then materia[9] else nil end
+    profesor = materia[10]
+    horas = materia[2..7]
+    horas.each_with_index do |h, i|
+      clases << clase(i+1,h,nombre,profesor) if h != "00-00"
+    end
+    clases
+  end
+
+  def self.clase(dia, horas, materia, profesor)
+    divididas = horas.split("-")
+    estructura = {
+      dia: dia,
+      hora_inicio: divididas[0],
+      hora_final: divididas[1],
+      materia: materia,
+      profesor: profesor
+    }
+    estructura
+  end
+end
