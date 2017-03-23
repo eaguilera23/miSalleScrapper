01a2fd0bf4bca0295c9aa10368e0e331d9389d73
Eduardo Aguilera <laloao.2302@gmail.com>
2017-03-23 16:37:30 -0600

    Agregar mensaje de error para contraseñas incorrectas

---
+++ app/helpers/error_helper.rb
@@ -0,0 +1,6 @@
+class ErrorHelper
+  def self.login
+    error = {codigo: 420, mensaje: "La matricula y/o contraseña son incorrectas."}
+    error
+  end
+end
--- app/router.rb
+++ app/router.rb
@@ -1,5 +1,6 @@
 require 'json'
 require_relative 'formateador'
+require_relative 'helpers/error_helper'
 class Router < Sinatra::Base
   set :server, 'webrick'
 
@@ -11,8 +12,6 @@ class Router < Sinatra::Base
     @json = JSON.parse(request.body.read)
     @matricula = @json["clave"]
     @password = @json["password"]
-    #@matricula = params["clave"]
-    #@password = params["password"]
 
     nav = Navegador.new(@matricula, @password)
     if nav.login then
@@ -20,14 +19,12 @@ class Router < Sinatra::Base
       content_type :json, :charset => 'utf-8'
       info.to_json
     else
-      "No man"
+      status 420
+      ErrorHelper.login.to_json
     end
   end
 
   get '/alumno' do
-    #@json = JSON.parse(request.body.read)
-    #@matricula = @json["clave"]
-    #@password = @json["password"]
     @matricula = params["clave"]
     @password = params["password"]
 
@@ -37,7 +34,8 @@ class Router < Sinatra::Base
       content_type :json, :charset => 'utf-8'
       info.to_json
     else
-      "No man"
+      status 420
+      ErrorHelper.login.to_json
     end
     # Modelos con sinatra
     # http://stackoverflow.com/questions/22597989/how-to-define-a-class-in-ruby-when-using-sequel
