b9f21683b9dea14037cf0e641f33be1067a5722a
Eduardo Aguilera <laloao.2302@gmail.com>
2017-07-27 12:04:04 -0500

    Add new available key

--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -4,7 +4,7 @@ module TokenHelper
   module_function
 
   $service_account_email = "firebase-adminsdk-dol2h@misalle-3ae7d.iam.gserviceaccount.com"
-  $private_key = OpenSSL::PKey::RSA.new(File.read(Dir.pwd + "/app/support/private.pem"))
+  $private_key = OpenSSL::PKey::RSA.new(File.read(Dir.pwd + "/app/support/new_private.pem"))
   
   def create_custom_token(matricula)
   now_seconds = Time.now.to_i
